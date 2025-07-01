112c64d467ff5842daaea66c878dbc7ef5f52cab
Arpita <arpitajana2000@gmail.com>
2025-07-01 22:26:43 +0530

    configs

--- backend/smart_task_tracker/settings.py
+++ backend/smart_task_tracker/settings.py
@@ -81,7 +81,10 @@ WSGI_APPLICATION = 'smart_task_tracker.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True)
 }
 
 # Custom user model
